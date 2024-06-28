7ac20be5acf4c4ffc1e71b868d7d78d49c9804ad
TTRademacher <rademacher.tim@gmail.com>
2024-06-27 21:39:45 -0400

    Create new graphic

--- 2_graphiques.R
+++ 2_graphiques.R
@@ -2,7 +2,7 @@
 # Produire des graphiques pour les donneés 
 #-------------------------------------------------------------------------------
 
-# lire les données ----
+# lire les données -------------------------------------------------------------
 source ("0_lire_données.R")
 
 # graphique du volume total de sève par traitement et arbre ----
@@ -10,12 +10,14 @@ source ("0_lire_données.R")
 par(mar = c(5, 5, 1, 1))
 plot(x = jitter(as.numeric(d_saison$t[d_saison$t == 2]), factor = 1),
      y = d_saison$vol_s[d_saison$t == 2],
-     xlim = c(0.5, 2.5), ylim = c(0, 25),
-     xlab = "Traitement", ylab = "Volume de sève (litres)",
+     xlim = c(0.5, 2.5), ylim = c(0, 60),
+     #xlab = "Traitement", ylab = "Volume de sève (litres)",
+     xlab = "Treatment", ylab = "Sap volume (litres)",
      axes = FALSE, pch = 19, col = "#67a9cfaa")
 points(x = jitter(as.numeric(d_saison$t[d_saison$t == 1]), factor = 1),
        y = d_saison$vol_s[d_saison$t == 1], pch = 19, col = "#ef8a62aa")
-axis(side = 1, at = 1:2, labels = c("Exclusion", "Témoin"))
+axis(side = 1, at = 1:2, labels = c("Exclusion", "Control"))
+#axis(side = 1, at = 1:2, labels = c("Exclusion", "Témoin"))
 axis(side = 2, las = 1)
 # ajoute l'écart type et les moyennes ----
 segments(x0 = mean(as.numeric(d_saison$t[d_saison$t == 1])) - 0.2,
@@ -39,30 +41,32 @@ points(x = mean(as.numeric(d_saison$t[d_saison$t == 2])),
 #-------------------------------------------------------------------------------
 par(mar = c(5, 5, 1, 1))
 plot(x = jitter(as.numeric(d$t[d$t == 2]), factor = 1),
-     y = d$Brix[d$t == 2],
-     xlim = c(0.5, 2.5), ylim = c(0, 6),
-     xlab = "Traitement", ylab = "Concentration de saccharose (°Brix)",
+     y = d$brix[d$t == 2],
+     xlim = c(0.5, 2.5), ylim = c(0, 4.5),
+     #xlab = "Traitement", ylab = "Concentration de saccharose (°Brix)",
+     xlab = "Treatment", ylab = "Estimated succrose concentration (°Brix)",
      axes = FALSE, pch = 19, col = "#67a9cf88", lwd = 1)
 points(x = jitter(as.numeric(d$t[d$t == 1]), factor = 1),
-       y = d$Brix[d$t == 1], pch = 19, col = "#ef8a6288", lwd = 1)
-axis(side = 1, at = 1:2, labels = c("Exclusion", "Témoin"))
+       y = d$brix[d$t == 1], pch = 19, col = "#ef8a6288", lwd = 1)
+#axis(side = 1, at = 1:2, labels = c("Exclusion", "Témoin"))
+axis(side = 1, at = 1:2, labels = c("Exclusion", "Control"))
 axis(side = 2, las = 1)
 # ajoute l'écart type et les moyennes ----
 segments(x0 = mean(as.numeric(d$t[d$t == 1])) - 0.2,
          x1 = mean(as.numeric(d$t[d$t == 1])) + 0.2,
-         y0 = mean(d$Brix[d$t == 1]),
-         y1 = mean(d$Brix[d$t == 1]),
+         y0 = mean(d$brix[d$t == 1], na.rm = TRUE),
+         y1 = mean(d$brix[d$t == 1], na.rm = TRUE),
          col = "#aaaaaa", lwd = 6)
 points(x = mean(as.numeric(d$t[d$t == 1])),
-       y = mean(d$Brix[d$t == 1]), 
+       y = mean(d$brix[d$t == 1], na.rm = TRUE), 
        col = "#aaaaaa", bg = "#ef8a62", lwd = 6, pch = 21, cex = 4)
 segments(x0 = mean(as.numeric(d$t[d$t == 2])) - 0.2,
          x1 = mean(as.numeric(d$t[d$t == 2])) + 0.2,
-         y0 = mean(d$Brix[d$t == 2], na.rm = TRUE),
-         y1 = mean(d$Brix[d$t == 2], na.rm = TRUE),
+         y0 = mean(d$brix[d$t == 2], na.rm = TRUE),
+         y1 = mean(d$brix[d$t == 2], na.rm = TRUE),
          col = "#aaaaaa", lwd = 6)
 points(x = mean(as.numeric(d$t[d$t == 2])),
-       y = mean(d$Brix[d$t == 2], na.rm = TRUE), 
+       y = mean(d$brix[d$t == 2], na.rm = TRUE), 
        col = "#aaaaaa", bg = "#67a9cf", pch = 21, cex = 4, lwd = 6)
 
 #===============================================================================
